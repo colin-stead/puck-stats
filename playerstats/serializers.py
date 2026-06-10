19405d0faa5f38acecaf80198728cedb9d9201ca
Colin Stead <colin.stead49@gmail.com>
2026-06-09 21:37:25 -0400

    Needs work, but use correct models/columns

--- playerstats/serializers.py
+++ playerstats/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Player, PlayerDescription, PlayerVideo, PlayerWeeklyAppearance
+from .models import Player, PlayerVideo, PlayerWeeklySnapshot
 
 
 class PlayerVideoSerializer(serializers.ModelSerializer):
@@ -8,67 +8,40 @@ class PlayerVideoSerializer(serializers.ModelSerializer):
         fields = ['youtube_url', 'title', 'display_order']
 
 
-class PlayerDescriptionSerializer(serializers.ModelSerializer):
+class PlayerWeeklySnapshotSerializer(serializers.ModelSerializer):
     class Meta:
-        model = PlayerDescription
-        fields = ['description', 'current_rank', 'previous_rank', 'iq_score_at_time',
-                  'consecutive_weeks_at_time', 'created_at']
-
-
-class PlayerWeeklyAppearanceSerializer(serializers.ModelSerializer):
-    week_number = serializers.IntegerField(source='week.week_number')
-    season = serializers.CharField(source='week.season')
-    start_date = serializers.DateField(source='week.start_date')
-
-    class Meta:
-        model = PlayerWeeklyAppearance
-        fields = ['week_number', 'season', 'start_date', 'ranking', 'iq_score', 'points']
+        model = PlayerWeeklySnapshot
+        fields = ['week', 'consecutive_weeks', 'current_ranking', 'previous_ranking',
+                  'goals', 'assists', 'points', 'plus_minus', 'created_at', 'updated_at']
 
 
 class PlayerListSerializer(serializers.ModelSerializer):
-    """Compact serializer for the rankings table."""
     position_category = serializers.CharField(read_only=True)
-    latest_description = serializers.SerializerMethodField()
 
     class Meta:
         model = Player
         fields = [
             'id', 'nhl_id', 'name', 'team', 'position', 'position_category',
-            'ranking', 'previous_ranking', 'iq_score',
-            'consecutive_weeks', 'games', 'goals', 'assists', 'points', 'plus_minus',
-            'headshot_url', 'latest_description',
+            'ranking', 'iq_score', 'games', 'goals', 'assists', 'points', 'plus_minus',
+            'headshot_url',
         ]
-
-    def get_latest_description(self, obj):
-        desc = obj.descriptions.first()
-        if desc:
-            return PlayerDescriptionSerializer(desc).data
-        return None
 
 
 class PlayerDetailSerializer(serializers.ModelSerializer):
-    """Full serializer for the player detail page."""
     position_category = serializers.CharField(read_only=True)
     videos = PlayerVideoSerializer(many=True, read_only=True)
-    latest_description = serializers.SerializerMethodField()
-    weekly_appearances = PlayerWeeklyAppearanceSerializer(many=True, read_only=True)
+    weekly_snapshots = PlayerWeeklySnapshotSerializer(many=True, read_only=True)
 
     class Meta:
         model = Player
         fields = [
             'id', 'nhl_id', 'name', 'number', 'team', 'position', 'position_category',
-            'ranking', 'previous_ranking', 'iq_score',
-            'consecutive_weeks', 'last_seen_week',
+            'ranking', 'iq_score',
             'games', 'goals', 'assists', 'primary_assists', 'points', 'plus_minus',
-            'time_on_ice_per_game', 'shots_on_goal', 'shooting_percentage',
-            'power_play_goals', 'power_play_points', 'short_handed_goals', 'game_winning_goals',
+            'time_on_ice_per_game',
             'points_per_60', 'primary_assists_per_60', 'plus_minus_per_60',
             'corsi_percentage', 'xgoals_percentage', 'zone_entry_success', 'defensive_zone_exits',
-            'headshot_url', 'videos', 'latest_description', 'weekly_appearances',
+            'description', 'headshot_url',
+            'videos', 'weekly_snapshots',
+            'created_at', 'updated_at',
         ]
-
-    def get_latest_description(self, obj):
-        desc = obj.descriptions.first()
-        if desc:
-            return PlayerDescriptionSerializer(desc).data
-        return None
